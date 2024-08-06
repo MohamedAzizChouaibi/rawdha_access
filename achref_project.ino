2a9137841655e871e11c84a6fefa6873089c6495
Mohamed Aziz Chouaibi <114361633+MohamedAzizChouaibi@users.noreply.github.com>
2024-08-06 17:49:09 +0100

    almost finish , missing some points

--- achref_project.ino
+++ achref_project.ino
@@ -1,12 +1,18 @@
 #include <SPI.h>
 #include <MFRC522.h>
 #include <LiquidCrystal_I2C.h>
-
+#include <ThreeWire.h>
+#include <RtcDS1302.h>
+ThreeWire myWire(5, 6, 7);        // DAT, CLK, RST
+RtcDS1302<ThreeWire> Rtc(myWire); 
 #define SS_PIN 10   // Slave Select pin
 #define RST_PIN 9   // Reset pin
 
 LiquidCrystal_I2C lcd(0x3F, 16, 2);
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+const int num_total = 20;
+
+int numVisitors = 490;
 
 class Visitor {
   public:
@@ -14,8 +20,10 @@ class Visitor {
     int age;
     String id;
     int number_of_visits = 0;
-    String last_visit = "";
-
+    int last_visit_day = 0;
+    int last_visit_month = 0;
+    int last_visit_year = 0;
+    
     Visitor() {
       name = "";
       age = 0;
@@ -28,10 +36,7 @@ class Visitor {
       id = id1;
     }
 };
-
-const int num_total = 20;
 Visitor myArr[num_total];
-int numVisitors = 499;
 
 void setup() {
   Serial.begin(9600);
@@ -56,46 +61,88 @@ bool test(String uid) {
       break;
     }
     i++;
-  }
+  } RtcDateTime now = Rtc.GetDateTime();
+
+  time_test:
+      if(myArr[i].number_of_visits>0){
+        
+      if (!(myArr[i].last_visit_year< now.Year() )) {
+        if(!(myArr[i].last_visit_month< now.Month())){
+          if (!(myArr[i].last_visit_month< now.Month())) {
+          lcd.setCursor(0, 0);
+        lcd.print(" you have to ");
+        lcd.setCursor(0, 1);
+        lcd.print("24 hours ");
+        delay(1500);  // Display message for 2 seconds
+        lcd.clear(); 
+        return false;
+          
+          }
+          }
+
+      }
+      }
+  numOfVisitor_test:
     if (numVisitors>500){
         lcd.setCursor(0, 0);
         lcd.print("there is no ");
         lcd.setCursor(0,1);
         lcd.print("place inside");
+        delay(1500);  // Display message for 2 seconds
+      lcd.clear();
         return false;
 
     }
-
-  if (i >= num_total) {
-    lcd.setCursor(0, 0);
-    lcd.print("Visitor not ");
-    lcd.setCursor(0,1);
-    lcd.print("registered");
-    return false;
-  }
-
-  if (myArr[i].number_of_visits >= 3) {
-    lcd.setCursor(0, 0);
-    lcd.print("Visitor had ");
-    lcd.setCursor(0,1);
-    lcd.print("3 visits");
-    return false;
-  }
-
-  // You can uncomment and adjust this if you want to use last_visit logic
-  // if (myArr[i].last_visit > 3) {
-  //   lcd.setCursor(0, 0);
-  //   lcd.print("Visitor end his 3 visits");
-  //   return false;
-  // }
+  VisitorRegistration_test:
+    if (i >= num_total) {
+      lcd.setCursor(0, 0);
+      lcd.print("Visitor not ");
+      lcd.setCursor(0,1);
+      lcd.print("registered");
+      delay(1500);  // Display message for 2 seconds
+        lcd.clear();
+      return false;
+    }
+  numOfVisits_test:
+    if (myArr[i].number_of_visits >= 3) {
+      lcd.setCursor(0, 0);
+      lcd.print("Visitor had ");
+      lcd.setCursor(0,1);
+      lcd.print("3 visits");
+      delay(1500);  // Display message for 2 seconds
+        lcd.clear();
+      return false;
+    }
+ 
 
   return true;
 }
 
+String getCardUID() {
+  String uidString = ""; 
+
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    if (mfrc522.uid.uidByte[i] < 0x10) {
+      uidString += "0";  // Add leading zero for bytes less than 0x10
+    }
+    uidString += String(mfrc522.uid.uidByte[i], HEX);  // Append byte in HEX format
+  }
+
+
+  uidString.toUpperCase();
+
+  Serial.println(uidString);
+
+  // Halt PICC communication
+  mfrc522.PICC_HaltA();
+  mfrc522.PCD_StopCrypto1();  // Ensure encryption is stopped
+
+  return uidString;  // Return the UID
+}
+
 void loop() {
   if (mfrc522.PICC_IsNewCardPresent()) {
-    Serial.println("New card detected");
-
+    
     if (mfrc522.PICC_ReadCardSerial()) {
       String uid = getCardUID();
       Serial.print("UID: ");
@@ -104,61 +151,31 @@ void loop() {
       if (test(uid)) {
         lcd.setCursor(0, 0);
         lcd.print("You can enter");
-        lcd.setCursor(0, 1);
-        lcd.print(uid);
-        numVisitors++;
-
-        int i = 0;
+         int i = 0;
         while (i < num_total) {
           if (myArr[i].id == uid) {
             break;
           }
           i++;
         }
-        if (i < num_total) { // Ensure index is within bounds
+          lcd.setCursor(0, 1);
+          lcd.print(myArr[i].name);
+          RtcDateTime now = Rtc.GetDateTime();
+          myArr[i].last_visit_day=now.Day();
+          myArr[i].last_visit_month=now.Month();
+          myArr[i].last_visit_year=now.Year();
+          numVisitors++;
+          
           myArr[i].number_of_visits++;
-        }
-      } else {
- 
-      }
+          
 
-      delay(2000);  // Display message for 2 seconds
+        
+
+      delay(1500);  // Display message for 2 seconds
       lcd.clear();  // Clear the display
-    } else {
-      Serial.println("Failed to read card serial");
-    }
-  } else {
-    Serial.println("No new card detected");
+      } 
+    
+    } 
   }
-
-  delay(1000);  // Delay to avoid constant reading
-}
-
-String getCardUID() {
-  String uidString = "";  // Variable to hold the UID
-
-  // Print debug information
-  Serial.print("Card UID Size: ");
-  Serial.println(mfrc522.uid.size);
-
-  // Convert UID to a string
-  for (byte i = 0; i < mfrc522.uid.size; i++) {
-    if (mfrc522.uid.uidByte[i] < 0x10) {
-      uidString += "0";  // Add leading zero for bytes less than 0x10
-    }
-    uidString += String(mfrc522.uid.uidByte[i], HEX);  // Append byte in HEX format
-  }
-
-  // Convert the UID to uppercase
-  uidString.toUpperCase();
-
-  // Print UID for debugging
-  Serial.print("UID String: ");
-  Serial.println(uidString);
-
-  // Halt PICC communication
-  mfrc522.PICC_HaltA();
-  mfrc522.PCD_StopCrypto1();  // Ensure encryption is stopped
-
-  return uidString;  // Return the UID
+ // Delay to avoid constant reading
 }
