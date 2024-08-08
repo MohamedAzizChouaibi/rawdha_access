c7bc301911d7723f6bbacbbfb9c24c0617770c65
Mohamed Aziz Chouaibi <114361633+MohamedAzizChouaibi@users.noreply.github.com>
2024-08-08 23:10:22 +0100

    Add files via upload

--- achref_project.ino
+++ achref_project.ino
@@ -1,14 +1,15 @@
-#include<Servo.h>
+#include <Servo.h>
 #include <SPI.h>
 #include <MFRC522.h>
 #include <LiquidCrystal_I2C.h>
 #include <ThreeWire.h>
 #include <RtcDS1302.h>
+
 // Pin definitions
 Servo outing_servo;
 #define SS_PIN 10   // Slave Select pin for RFID
 #define RST_PIN 9   // Reset pin for RFID
-int outing_sensor = 4 ;
+int outing_sensor = 4;
 // RTC DS1302 Wiring: DAT, CLK, RST
 ThreeWire myWire(5, 6, 7);
 RtcDS1302<ThreeWire> Rtc(myWire);
@@ -22,12 +23,10 @@ MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 // Maximum number of visitors
 const int num_total = 20;
 const int numVisitorsInsideMax = 2;
-int numVisitorsInside = 0;
-int j=0;
 
 // Visitor class to store visitor information
 class Visitor {
-public:
+  public:
     String name;
     int age;
     String id;
@@ -52,13 +51,14 @@ public:
 
 // Arrays to store all visitors and those inside
 Visitor allvisitor[num_total];
+int numVisitorsInside = 0;
 
-String visitorInside[numVisitorsInsideMax];
 void setup() {
-  outing_servo.attach(3);
+    outing_servo.attach(3);
+
     Serial.begin(9600);
-    
-    pinMode(4,INPUT);
+
+    pinMode(outing_sensor, INPUT);
     SPI.begin();
     mfrc522.PCD_Init();
 
@@ -66,23 +66,14 @@ void setup() {
     lcd.init();
     lcd.backlight();
 
-
-
-  Visitor v1("folen1", 22, "793C097F"); // First visitor
-  Visitor v2("folen2", 30, "FB17AB1B");
-  Visitor v3("folen3", 17, "899BEAC9");
-allvisitor[0] = v1;
-allvisitor[1] = v2; 
-
+    Visitor v1("folen1", 22, "793C097F"); // First visitor
+    Visitor v2("folen2", 30, "FB17AB1B");
+    Visitor v3("folen3", 12, "899BEAC9");
+    allvisitor[0] = v1;
+    allvisitor[1] = v2;
+    allvisitor[2] = v3;
 }
-bool isElementInArray(String element, String array[], int size) {
-  for (int i = 0; i < size; i++) {
-    if (array[i] == element) {
-      return true; // Element found
-    }
-  }
-  return false; // Element not found
-}
+
 bool test(String uid) {
     Serial.println("Checking UID against registered visitors...");
 
@@ -94,8 +85,8 @@ bool test(String uid) {
         i++;
     }
 
-    // If no matching UID is found
-    if (allvisitor[i].id != uid) {
+    // If no matching UID is found or i is out of range
+    if (i >= num_total || allvisitor[i].id != uid) {
         lcd.setCursor(0, 0);
         lcd.print("Visitor not ");
         lcd.setCursor(0, 1);
@@ -108,20 +99,6 @@ bool test(String uid) {
     // Get current date and time
     RtcDateTime now = Rtc.GetDateTime();
 
-
-     
-      if(isElementInArray(uid, visitorInside, numVisitorsInsideMax)){
-        lcd.setCursor(0, 0);
-        lcd.print("Visitor is  ");
-        lcd.setCursor(0, 1);
-        lcd.print("inside");
-        delay(1500);
-        lcd.clear();
-        return false;
-      }
-    
-
-
     if (allvisitor[i].number_of_visits >= 3) {
         lcd.setCursor(0, 0);
         lcd.print("Visitor had ");
@@ -131,6 +108,7 @@ bool test(String uid) {
         lcd.clear();
         return false;
     }
+
     // Check if the visitor has to wait for a certain period
     if (allvisitor[i].number_of_visits > 0) {
         if (abs(allvisitor[i].last_visit_minute - now.Minute()) < 1) {
@@ -144,13 +122,18 @@ bool test(String uid) {
         }
     }
 
-  if (numVisitorsInside >= numVisitorsInsideMax) {
+    Serial.print("numVisitorsInside: ");
+    Serial.println(numVisitorsInside);
+
+    if (numVisitorsInside >= numVisitorsInsideMax) {
         lcd.setCursor(0, 0);
         lcd.print("There is no ");
         lcd.setCursor(0, 1);
         lcd.print("place inside");
         delay(1500);
         lcd.clear();
+        Serial.print("numVisitorsInside: ");
+        Serial.println(numVisitorsInside);
         return false;
     }
     
@@ -209,31 +192,17 @@ void loop() {
                 // Update visitor inside info
                 numVisitorsInside++;
                 allvisitor[i].number_of_visits++;
-                visitorInside[j]=allvisitor[i].id;
-                j++;
 
-                delay(1500);  // Display message for 2 seconds
+                delay(1500);  // Display message for 1.5 seconds
                 lcd.clear();  // Clear the display
             }
         }
     }
-    if(digitalRead(outing_sensor )==1){
-numVisitorsInside-- ;
-outing_servo.write(90) ;
-delay(100);}
 
-
+    if (digitalRead(outing_sensor) == 0 && numVisitorsInside > 0) {
+        numVisitorsInside--;
+        outing_servo.write(90);
+        delay(1000);
+        outing_servo.write(0);
     }
-/*    void test_outing(bool isElementInArray,( String uid,  String visitorInside, int numVisitorsInsideMax)){
-      if(isElementInArray(uid, visitorInside, numVisitorsInsideMax)){
-        lcd.setCursor(0, 0);
-        lcd.print("Visitor is  ");
-        lcd.setCursor(0, 1);
-        lcd.print("inside");
-        delay(1500);
-        lcd.clear();
-        return false;
-      }
-      
-    }*/
-
+}
